d23cbca1333b3a89b7ef358a0d400405e1099870
Masfarre <jmasmer268@g.educaand.es>
2024-09-27 21:30:17 +0200

    ejercicio 7 al ejercicio 9

---
+++ src/ejercicio07.py
@@ -0,0 +1,5 @@
+num1 = float(input("Introduce un numero:"))
+num2 = float(input("Introduce un numero:"))
+num3 = float(input("Introduce un numero:"))
+
+print("La suma de los 3 numeros introducidos es = ", num1 + num2 + num3)
---
+++ src/ejercicio08.py
@@ -0,0 +1,8 @@
+num1 = float(input("Introduce un numero:"))
+num2 = num1
+num1 = float(input("Introduce un numero:"))
+num2 += num1
+num1 = float(input("Introduce un numero:"))
+num2 += num1
+
+print("La suma de los tres numeros es =  ", num2)
