a8cfa484234acd9a065f638c262872674b2edd0e
Masfarre <jmasmer268@g.educaand.es>
2024-09-30 21:37:53 +0200

    Actualizacion

--- src/ejercicio07.py
+++ src/ejercicio07.py
@@ -1,5 +1,5 @@
-num1 = float(input("Introduce un numero:"))
-num2 = float(input("Introduce un numero:"))
-num3 = float(input("Introduce un numero:"))
+num1 = float(input("Introduce un numero: "))
+num2 = float(input("Introduce un numero: "))
+num3 = float(input("Introduce un numero: "))
 
-print("La suma de los 3 numeros introducidos es = ", num1 + num2 + num3)
+print("La suma de los 3 numeros introducidos es =",num1 + num2 + num3)
