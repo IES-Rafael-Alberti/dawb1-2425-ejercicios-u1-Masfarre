8ad43d5336ebc87d29efefb05c25cfe173c961b3
Masfarre <jmasmer268@g.educaand.es>
2024-10-08 16:50:03 +0200

    Actualizacion pytest prueba 1

--- tests/Test_ej01_def.py
+++ tests/Test_ej01_def.py
@@ -2,8 +2,13 @@ import pytest
 from src.ejercicio01_def import introduce_nombre
 
 @pytest.mark.parametrize(
-
+    "input_x, expected",
+    [
+        ("     fwasd","fwasd"),
+        ("gh    ","gh"),
+        ("      hh    ","hh")
+    ]
 )
 
-def test_nombre_paramts():
-    assert introduce_nombre() == expected
+def test_mombre_params(input_x, expected):
+    assert introduce_nombre(input_x) == expected
